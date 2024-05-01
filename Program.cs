7e4a3bc0a2a2624621bab93730c75bc7b3bb5d8a
Rizwan Haider <rizuu004@gmail.com>
2024-05-01 18:13:32 +0500

    Input/Output
    Basic Input/Output operations in C#

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Name");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Enter a Registration Number");
+            String reg = Console.ReadLine();
+
+            Console.WriteLine("Enter age");
+            double age = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a Blood Gropu");
+            String bg = Console.ReadLine();
+
+            Console.WriteLine("Favorite number");
+            int fav = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a Favourite Character");
+            char fav_char = Char.Parse(Console.ReadLine());
+
+            Console.WriteLine("/----Details-----/");
+            Console.WriteLine("Name is               :"+name);
+            Console.WriteLine("Registration number   :"+reg);
+            Console.WriteLine("Age is                :"+age);
+            Console.WriteLine("Blood Group is        :"+bg);
+            Console.WriteLine("Favourite Number      :"+fav);
+            Console.WriteLine("Favourite Character   :"+fav_char);
+
+
+
+
+        }
+    }
+}
